54a35ce2d534314df079d9528eb29d9115782115
MARIANA FERNANDES ROCHA <B51099@fgv.edu.br>
2023-10-30 08:38:19 -0300

    Criando outras categorias

--- product.py
+++ product.py
@@ -10,7 +10,16 @@ class Produto:
         qtd_prod += 1 
 
 class CathegoryTech(Produto):
-    def __init__(self, categoria = "Tecnologia")
+    def __init__(self, barcode, preco, marca, categoria):
+        super().__init__(barcode, preco, marca, categoria = "Tecnologia")
+
+class CathegoryBeauty(Produto):
+    def __init__(self, barcode, preco, marca, categoria):
+        super().__init__(barcode, preco, marca, categoria = "Beleza")
+
+class CathegoryCloth(Produto):
+    def __init__(self, barcode, preco, marca, categoria):
+        super().__init__(barcode, preco, marca, categoria = "Vestuário")
 
 
 class Inventario():
