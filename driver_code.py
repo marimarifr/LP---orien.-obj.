cba6cf18c3ad41acc96ff736f96c0e118bb093d8
Eduarda Mesquita <eduardammag@gmail.com>
2023-10-30 16:49:31 -0300

    changes

--- driver_code.py
+++ driver_code.py
@@ -1,5 +1,5 @@
 from product import Produto, Marcas
-from categoria import CathegoryBeauty, CathegoryCloth, CathegoryTech
+from Inventario import CathegoryBeauty, CathegoryCloth, CathegoryTech
 
 tablet = CathegoryTech("Tablet", 1235, 1000.00, Marcas.Amazon)
 batom = CathegoryBeauty("Batom", 2455, 55.26, Marcas.Avon)
